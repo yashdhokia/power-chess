f502f6a7dd120bbb3aba62804f12a4b9dcd51f89
Yash Dhokia <yashdhokia@gmail.com>
2024-12-03 09:56:10 +0530

    Add drag and drop

--- frontend/src/components/ChessBoardView.vue
+++ frontend/src/components/ChessBoardView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import socket from "../../plugins/socket.js";
-import { io } from "socket.io-client";
 const column = ref<string[]>(["8", "7", "6", "5", "4", "3", "2", "1"]);
 const row = ref<string[]>(["a", "b", "c", "d", "e", "f", "g", "h"]);
+type DraggedItem = { square: string; piece: string | null };
+const draggedItem = ref<DraggedItem>({ square: "", piece: null });
+const sourceContainer = ref<string>([]);
+type ChessBoard = Record<string, null>;
+const chessMetric = ref<ChessBoard>({});
 const blackKing = ref<string>("src/assets/chessPieces/black_king.svg");
 const whiteKing = ref<string>("src/assets/chessPieces/white_king.svg");
 const blackQueen = ref<string>("src/assets/chessPieces/black_queen.svg");
@@ -16,43 +20,59 @@ const blackBishop = ref<string>("src/assets/chessPieces/black_bishop.svg");
 const whiteBishop = ref<string>("src/assets/chessPieces/white_bishop.svg");
 const blackPawn = ref<string>("src/assets/chessPieces/black_pawn.svg");
 const whitePawn = ref<string>("src/assets/chessPieces/white_pawn.svg");
-function setPeice(row: string, col: string){
-    const colNumber:number = Number(col);
-    if(colNumber === 2) 
-        return whitePawn.value;
-    else if(colNumber === 7)
-        return blackPawn.value;
-    const index = row+col;
-    if(index === 'a1' || index === 'h1')
-        return whiteRook.value;
-    if(index === 'b1' || index === 'g1')
-        return whiteKnight.value;
-    if(index === 'c1' || index === 'f1')
-        return whiteBishop.value;
-    if(index === 'a8' || index === 'h8')
-        return blackRook.value;
-    if(index === 'b8' || index === 'g8')
-        return blackKnight.value;
-    if(index === 'c8' || index === 'f8')
-        return blackBishop.value;
-    if(index === 'd1')
-        return whiteQueen.value;
-    if(index === 'e1')
-        return whiteKing.value;
-    if(index === 'd8')
-        return blackQueen.value;
-    if(index === 'e8')
-        return blackKing.value;
-    return
-}
-const startDrag = (event) => {
-  console.log('==============>'); 
-}
-const onDrop = (event) => {
 
-}
-socket.on('playerRole', (playerRole) => console.log(playerRole));
+  setInitialBoard(chessMetric);
 
+function setInitialBoard() {
+  chessMetric.value.a1 = whiteRook.value;
+  chessMetric.value.h1 = whiteRook.value;
+  chessMetric.value.b1 = whiteKnight.value;
+  chessMetric.value.g1 = whiteKnight.value;
+  chessMetric.value.c1 = whiteBishop.value;
+  chessMetric.value.f1 = whiteBishop.value;
+  chessMetric.value.a8 = blackRook.value;
+  chessMetric.value.h8 = blackRook.value;
+  chessMetric.value.b8 = blackKnight.value;
+  chessMetric.value.g8 = blackKnight.value;
+  chessMetric.value.c8 = blackBishop.value;
+  chessMetric.value.f8 = blackBishop.value;
+  chessMetric.value.d1 = whiteQueen.value;
+  chessMetric.value.e1 = whiteKing.value;
+  chessMetric.value.d8 = blackQueen.value;
+  chessMetric.value.e8 = blackKing.value;
+
+  chessMetric.value.a2 = whitePawn.value;
+  chessMetric.value.b2 = whitePawn.value;
+  chessMetric.value.c2 = whitePawn.value;
+  chessMetric.value.d2 = whitePawn.value;
+  chessMetric.value.e2 = whitePawn.value;
+  chessMetric.value.f2 = whitePawn.value;
+  chessMetric.value.g2 = whitePawn.value;
+  chessMetric.value.h2 = whitePawn.value;
+
+  chessMetric.value.a7 = blackPawn.value;
+  chessMetric.value.b7 = blackPawn.value;
+  chessMetric.value.c7 = blackPawn.value;
+  chessMetric.value.d7 = blackPawn.value;
+  chessMetric.value.e7 = blackPawn.value;
+  chessMetric.value.f7 = blackPawn.value;
+  chessMetric.value.g7 = blackPawn.value;
+  chessMetric.value.h7 = blackPawn.value;
+}
+function startDrag(startSquare: string, pieceInfo: string) {
+  draggedItem.value.square = startSquare;
+  draggedItem.value.piece = pieceInfo;
+}
+
+function onDrop(targetSquare: string) {
+  if (draggedItem.value.square && draggedItem.value.square !== targetSquare) {
+    chessMetric.value[targetSquare] = draggedItem.value.piece || ""; // Move the piece to the target
+    chessMetric.value[draggedItem.value.square] = ""; // Clear the starting square
+  }
+
+  // Reset dragged item
+  draggedItem.value = { square: "", piece: null };
+}
 </script>
 
 <template>
@@ -60,22 +80,24 @@ socket.on('playerRole', (playerRole) => console.log(playerRole));
     <div v-for="(col, colIndex) in column" :key="col + colIndex" class="cell">
       <div
         v-for="(row, rowIndex) in row"
-        :key="col+row"
-        :id="col+row"
+        :key="col + row"
+        :id="col + row"
         :class="['row', (rowIndex + colIndex) % 2 === 0 ? 'white' : 'black']"
-        :style="{color:(rowIndex + colIndex) % 2 === 0 ? '#5e8cad' : '#fff'}"
-        @drop="onDrop($event)"
-        @dropenter.prevent
-        @dropover.prevent
+        :style="{ color: (rowIndex + colIndex) % 2 === 0 ? '#5e8cad' : '#fff' }"
+        @dragover.prevent
+        @drop="onDrop(row + col)"
       >
-          <div v-if="row === 'a'" :class="['col-label']">
-            {{ col }}
-          </div>
-          <div v-if="col === '1'" :class="['row-label']">
-            {{ row }}
-          </div>
-          <img :src="setPeice(row,col)" :draggable="true" @dragstart="startDrag($event)"/>
-          
+        <div v-if="row === 'a'" :class="['col-label']">
+          {{ col }}
+        </div>
+        <div v-if="col === '1'" :class="['row-label']">
+          {{ row }}
+        </div>
+        <img
+          :src="chessMetric[row + col]"
+          :draggable="true"
+          @dragstart="startDrag(row + col, chessMetric[row + col])"
+        />
       </div>
     </div>
   </div>
@@ -87,7 +109,7 @@ socket.on('playerRole', (playerRole) => console.log(playerRole));
   flex-direction: column;
   width: 700px; /* Adjust size */
   /* height: 390px; */
-  color:black;
+  color: black;
   font-size: 100%;
   font-weight: bold;
 }
@@ -98,8 +120,8 @@ socket.on('playerRole', (playerRole) => console.log(playerRole));
 }
 
 .row {
-    position: relative;
-    display: flex;
+  position: relative;
+  display: flex;
   flex: 1;
   aspect-ratio: 1; /* Make cells square */
 }
@@ -113,18 +135,17 @@ socket.on('playerRole', (playerRole) => console.log(playerRole));
 }
 
 .col-label {
-    position: absolute;
+  position: absolute;
   justify-self: flex-start;
   /* opacity: 0.4; */
   margin-right: 5%;
-  margin-left:5%;
+  margin-left: 5%;
 }
 
 .row-label {
-    position: absolute;
-    align-self: flex-end;
+  position: absolute;
+  align-self: flex-end;
   /* opacity: 0.4; */
   right: 5%;
 }
-
 </style>
